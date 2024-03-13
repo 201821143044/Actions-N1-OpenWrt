1ceb7a9b30d5a5b8ce77dff2267f71383ddcf794
201821143044 <49841556+201821143044@users.noreply.github.com>
2024-03-13 12:35:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,11 @@ sed -i 's#192.168.1.1#192.168.1.99#g' package/base-files/files/bin/config_genera
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
 sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
+# 修改golang源码以编译xray1.8.8+版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
+
 # 跟随最新版naiveproxy
 rm -rf feeds/passwall_packages/naiveproxy
 rm -rf feeds/helloworld/naiveproxy
