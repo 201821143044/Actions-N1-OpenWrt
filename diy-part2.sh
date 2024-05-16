d539640a7882e6b743c176a71c528e9020b1a22d
201821143044 <49841556+201821143044@users.noreply.github.com>
2024-05-16 12:38:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,9 +23,9 @@ sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
 sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.58.0/' feeds/packages/net/frp/Makefile
 sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=2428ed4d9df6f2be29d006c5fcdeb526b86a137fa007a396af9b9d28ea3cee60/' feeds/packages/net/frp/Makefile
 
-# 修改tailscale版本为官网最新v1.66.1 https://github.com/tailscale/tailscale 格式：https://codeload.github.com/tailscale/tailscale/tar.gz/v$(PKG_VERSION)?
-sed -i 's/PKG_VERSION:=1.66.1/PKG_VERSION:=1.66.1/' feeds/packages/net/tailscale/Makefile
-sed -i 's/PKG_HASH:=a3c8645891d2dd25ad417df16e7f635cdf98d2c01778614942c6e39218c84a65/PKG_HASH:=a3c8645891d2dd25ad417df16e7f635cdf98d2c01778614942c6e39218c84a65/' feeds/packages/net/tailscale/Makefile
+# 修改tailscale版本为官网最新v1.66.2 https://github.com/tailscale/tailscale 格式：https://codeload.github.com/tailscale/tailscale/tar.gz/v$(PKG_VERSION)?
+sed -i 's/PKG_VERSION:=1.66.1/PKG_VERSION:=1.66.2/' feeds/packages/net/tailscale/Makefile
+sed -i 's/PKG_HASH:=a3c8645891d2dd25ad417df16e7f635cdf98d2c01778614942c6e39218c84a65/PKG_HASH:=26dd40e60848710f4fd77f8d1191ae8ad506c710ad471723af51da807375a638/' feeds/packages/net/tailscale/Makefile
 rm -rf feeds/packages/net/tailscale/patches
 
 # 跟随最新版naiveproxy
